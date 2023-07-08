991ccac28d9afb242eabfa554fc0f758d8571889
jchugh01 <jatinchugh.btcse20@pec.edu.in>
2023-07-08 20:05:23 +0530

    Header andf footer

--- cfg/src/App.js
+++ cfg/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './header';
+import Footer from './footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img className="App-logo" alt="logo" />
         <p>
@@ -17,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
