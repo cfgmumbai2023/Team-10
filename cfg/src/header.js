4e21a94728c0d3caeb30dc8b164bb2e2a4712d9f
jchugh01 <jatinchugh.btcse20@pec.edu.in>
2023-07-08 23:31:00 +0530

    Header styling

--- cfg/src/header.js
+++ cfg/src/header.js
@@ -1,40 +1,23 @@
-import React from 'react';
 import './header.css';
-import { Navbar, Nav, FormControl, Button, Form, Dropdown } from 'react-bootstrap';
+import { FormControl, Button, Form } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
-const Header = () => {
-    return (
-      <Navbar bg="light" className='nav'>
-        <img src='https://rkssngo.org/wp-content/uploads/2021/02/cropped-logo-rkss-org.png' alt='RKSS' className='logo' />
-        <Navbar.Brand className='ngoName'>RKSS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            {/* Add more navigation links here */}
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="searchBar" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          <Dropdown>
-            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-              Dropdown
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item href="#action1">Action 1</Dropdown.Item>
-              <Dropdown.Item href="#action2">Action 2</Dropdown.Item>
-              {/* Add more dropdown items here */}
-            </Dropdown.Menu>
-          </Dropdown>
-        </Navbar.Collapse>
-        <Link to="/login"><Button >Login</Button></Link>
-        <Link to="/signin"><Button >Signin</Button></Link>
-        
-      </Navbar>
-    );
-  };
+import React from 'react';
 
-  export default Header;
+const Header = () => {
+  return (
+    <header className="header">
+      <img src='https://rkssngo.org/wp-content/uploads/2021/02/cropped-logo-rkss-org.png' className="logo" alt='RKSS' />
+    <Form inline>
+        <FormControl type="text" placeholder="Search" className="searchBar" />
+        <Button variant="outline-success">Search</Button>
+    </Form>
+    <div>
+      <Link to="/login"><Button >Login</Button></Link>
+      <Link to="/signin"><Button >Signin</Button></Link>
+    </div>
+    </header>
+  );
+};
+
+export default Header;
