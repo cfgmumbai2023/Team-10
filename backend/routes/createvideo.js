0fc12865ebb5893a9956b91470bfc8918b96547f
bandivamsikrishnaeddy <109067546+bandivamsikrishnaeddy@users.noreply.github.com>
2023-07-09 06:37:48 +0530

    redited

--- backend/routes/createvideo.js
+++ backend/routes/createvideo.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const route = express.Router()
+const route = express.Router();
 
 //importing the controllers
 const { createVideo, getVideoByTag, filter } = require("../controller/video")
 //post request
 route.post("/createVideo", createVideo);
-route.post("/getbyTag", getVideoByTag);
+route.get("/getbyTag", getVideoByTag);
 route.get('/getbyfilter', filter);
 
 module.exports = route 
